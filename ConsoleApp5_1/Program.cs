af0c807ecaa1ab3a403541d53e57c60d5f1d1062
Ustyugova Julia <bublikjulija@rambler.ru>
2019-08-25 18:03:35 +0600

    методы

--- ConsoleApp5_1/Program.cs
+++ ConsoleApp5_1/Program.cs
@@ -11,6 +11,31 @@ namespace ConsoleApp5_1
 
         static void Main(string[] args)
         {
+
+            string[] cardSuits = CardSuitsInizialization();
+
+            int[] cardIndex = CardIndexInizialization();
+
+            RandomBubble(cardIndex);
+            DealCards(cardIndex, cardSuits);
+            SortingCard(cardIndex, cardSuits);
+            Console.ReadKey();
+
+        }
+
+        private static int[] CardIndexInizialization()
+        {
+            int[] cardIndex = new int[52];
+            int j = 52;
+            for (int i = 0; i < j; i++)
+            {
+                cardIndex[i] = i;
+            }
+            return cardIndex;
+        }
+
+        private static string[] CardSuitsInizialization()
+        {
             string[] cardSuits = new string[52];
             cardSuits[0] = "10-baptize";
             cardSuits[1] = "9-baptize";
@@ -64,67 +89,7 @@ namespace ConsoleApp5_1
             cardSuits[49] = "K-peaks";
             cardSuits[50] = "Q-peaks";
             cardSuits[51] = "J-peaks";
-
-            int[] cardIndex = new int[52];
-            cardIndex[0] = 0;
-            cardIndex[1] = 1;
-            cardIndex[2] = 2;
-            cardIndex[3] = 3;
-            cardIndex[4] = 4;
-            cardIndex[5] = 5;
-            cardIndex[6] = 6;
-            cardIndex[7] = 7;
-            cardIndex[8] = 8;
-            cardIndex[9] = 9;
-            cardIndex[10] = 10;
-            cardIndex[11] = 11;
-            cardIndex[12] = 12;
-            cardIndex[13] = 13;
-            cardIndex[14] = 14;
-            cardIndex[15] = 15;
-            cardIndex[16] = 16;
-            cardIndex[17] = 17;
-            cardIndex[18] = 18;
-            cardIndex[19] = 19;
-            cardIndex[20] = 20;
-            cardIndex[21] = 21;
-            cardIndex[22] = 22;
-            cardIndex[23] = 23;
-            cardIndex[24] = 24;
-            cardIndex[25] = 25;
-            cardIndex[26] = 26;
-            cardIndex[27] = 27;
-            cardIndex[28] = 28;
-            cardIndex[29] = 29;
-            cardIndex[30] = 30;
-            cardIndex[31] = 31;
-            cardIndex[32] = 32;
-            cardIndex[33] = 33;
-            cardIndex[34] = 34;
-            cardIndex[35] = 35;
-            cardIndex[36] = 36;
-            cardIndex[37] = 37;
-            cardIndex[38] = 38;
-            cardIndex[39] = 39;
-            cardIndex[40] = 40;
-            cardIndex[41] = 41;
-            cardIndex[42] = 42;
-            cardIndex[43] = 43;
-            cardIndex[44] = 44;
-            cardIndex[45] = 45;
-            cardIndex[46] = 46;
-            cardIndex[47] = 47;
-            cardIndex[48] = 48;
-            cardIndex[49] = 49;
-            cardIndex[50] = 50;
-            cardIndex[51] = 51;
-
-
-            RandomBubble(cardIndex);
-            DealCards(cardIndex, cardSuits);
-            SortingCard(cardIndex, cardSuits);
-            Console.ReadKey();
-
+            return cardSuits;
         }
 
         private static void SortingCard(int[] cardIndex, String[] cardSuits)
@@ -148,7 +113,7 @@ namespace ConsoleApp5_1
                         }
                     }
                 }
-                Console.WriteLine($"Игрок номер {t}");
+                Console.WriteLine($"Игрок номер {t+1}");
                 for (var k = start; k < start + 5; k++)
                 {
 
